5ba41536cf32012d4039f319cbab45722c6af482
sugandv21 <sdvsuganya@gmail.com>
2025-08-20 11:59:41 +0530

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify
+from datetime import datetime
+
+app = Flask(__name__, template_folder="../frontend")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/time")
+def get_time():
+    now = datetime.now().strftime("%H:%M:%S")
+    return jsonify({"time": now})
+
+if __name__ == "__main__":
+    app.run(debug=True)
